ff625713f9add5a467bbc4451a02ff51f47b1e73
lavlo <lavlovandrejj@gmail.com>
2022-12-01 20:40:43 +0400

    start

---
+++ src/test/java/com/company/klavr/KlavrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.klavr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlavrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
