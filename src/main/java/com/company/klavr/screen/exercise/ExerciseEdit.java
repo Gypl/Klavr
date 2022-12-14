12371bdd5d85762644689f3434a3275f328f445d
Danila <danilakosarev8@gmail.com>
2022-12-14 23:54:01 +0400

    kto ne commitit tot lox 222

--- src/main/java/com/company/klavr/screen/exercise/ExerciseEdit.java
+++ src/main/java/com/company/klavr/screen/exercise/ExerciseEdit.java
@@ -1,10 +1,19 @@
 package com.company.klavr.screen.exercise;
 
+import io.jmix.ui.component.TextField;
+import io.jmix.ui.component.TextInputField;
 import io.jmix.ui.screen.*;
 import com.company.klavr.entity.Exercise;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("Exercise.edit")
 @UiDescriptor("exercise-edit.xml")
 @EditedEntityContainer("exerciseDc")
 public class ExerciseEdit extends StandardEditor<Exercise> {
+    @Subscribe("textField")
+    public void onTextFieldTextChange(TextInputField.TextChangeEvent event) {
+        lengthField.setValue(event.getText().length());
+    }
+    @Autowired
+    private TextField<Integer> lengthField;
 }
