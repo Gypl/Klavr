394bd997435c21836bfc8c049082c7e399d33d1e
Mix1e <109144752+Mix1e@users.noreply.github.com>
2022-12-15 23:04:46 +0400

    выполнение упражнения готово

--- src/main/java/com/company/klavr/screen/exercise/ExerciseScreen.java
+++ src/main/java/com/company/klavr/screen/exercise/ExerciseScreen.java
@@ -1,5 +1,8 @@
 package com.company.klavr.screen.exercise;
 
+import com.company.klavr.entity.Statistics;
+import com.company.klavr.logic.ExerciseHandler;
+import io.jmix.core.DataManager;
 import io.jmix.ui.component.*;
 import io.jmix.ui.screen.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 @UiDescriptor("Exercise-screen.xml")
 public class ExerciseScreen extends Screen {
 
+    private String written = "";
+    private String needed = "";
+    private int timerValue = 0;
+    private boolean isTimer = false;
+    private ExerciseHandler exerciseHandler;
+    private boolean canExecution = true;
     @Autowired
     private Label writtenText;
     @Autowired
@@ -18,10 +27,22 @@ public class ExerciseScreen extends Screen {
     private CheckBox keyboardCheckBox;
     @Autowired
     private CssLayout keyboardBox;
-    private String written = "";
-    private String needed = "";
+
+    @Autowired
+    private DataManager dataManager;
+    @Autowired
+    private Timer timer;
+    @Autowired
+    private Label lengthLabel;
+    @Autowired
+    private Label mistakesLabel;
+    @Autowired
+    private Label timeLabel;
+    @Autowired
+    private MessageDialogFacet messageDialog;
 
     public ExerciseScreen() {
+        exerciseHandler = new ExerciseHandler(255, 10);
         needed = "тест очка";
     }
 
@@ -29,8 +50,21 @@ public class ExerciseScreen extends Screen {
     public void onInit(InitEvent event) {
         needed = needed.replaceAll(" ", "\u00A0");
         text.setValue(needed);
+        timeLabel.setValue(String.format(" %d сек", 0));
+        lengthLabel.setValue(String.format(" 0/%d", exerciseHandler.getMaxLength()));
+        mistakesLabel.setValue(String.format(" 0/%d", exerciseHandler.getMaxMistakes()));
 
         keyboardBox.setVisible(keyboardCheckBox.isChecked());
+    }
+
+    private void saveStatistics() {
+        Statistics statistics = dataManager.create(Statistics.class);
+        /*statistics.setStatistics_to_exercise(dataManager.load);
+        statistics.setStatistics_to_user();
+
+        statistics.setFinishDate();
+
+        dataManager.save(statistics);*/
     }
 
     private void showKeyPress(char clicked) {
@@ -38,9 +72,10 @@ public class ExerciseScreen extends Screen {
             component -> {
                 if(component.toString().contains("Button")) {
                     Button button = (Button) component;
-                    if (button.getCaption().toUpperCase() == String.valueOf(clicked).toUpperCase()) {
-                        button.click();
+                    if (button.getCaption().trim().equals((clicked+"").toUpperCase())) {
+                        System.out.println(clicked);
                     }
+
                 }
             }
         );
@@ -51,6 +86,11 @@ public class ExerciseScreen extends Screen {
         keyboardBox.setVisible(event.getValue());
     }
 
+    @Subscribe("timer")
+    public void onTimerTimerAction(Timer.TimerActionEvent event) {
+        timeLabel.setValue(String.format(" %d сек", ++timerValue));
+    }
+
     @Subscribe("anton")
     public void onAntonValueChange(HasValue.ValueChangeEvent event) {
         if(anton.getRawValue() == "") {
@@ -59,15 +99,37 @@ public class ExerciseScreen extends Screen {
         if (needed == "") {
             return;
         }
+        if (!isTimer) {
+            timer.start();
+            this.isTimer = true;
+        }
+
+        if (!canExecution)
+            return;
+
         handleInput();
     }
 
     private void exerciseDone() {
         System.out.println("Well done");
+        executionStop();
+    }
+
+    private void executionStop() {
+        timer.stop();
+        canExecution = false;
+        if (timerValue != 0) {
+            exerciseHandler.setAverageSpeed(Math.round(exerciseHandler.getCurrentLength()/timerValue));
+        }
     }
 
     private void wrongSymbol() {
-        System.err.println("Err");
+        exerciseHandler.mistake();
+        mistakesLabel.setValue(String.format(" %d/%d", exerciseHandler.getCurrentMistakes(), exerciseHandler.getMaxMistakes()));
+        if (exerciseHandler.isMistakesFull()) {
+            executionStop();
+            System.err.println("You lose!");
+        }
     }
 
     private void handleInput() {
@@ -93,7 +155,24 @@ public class ExerciseScreen extends Screen {
             text.setValue(needed);
             written += changed == ' ' ? "\u00A0":changed;
             writtenText.setValue(written);
+
+            exerciseHandler.incLength();
+            lengthLabel.setValue(String.format(" %d/%d", exerciseHandler.getCurrentLength(), exerciseHandler.getMaxLength()));
         }
+    }
+
+    @Subscribe("completeButton")
+    public void onCompleteButtonClick(Button.ClickEvent event) {
+        executionStop();
+        messageDialog.setMessage(getFinalMessage());
+        messageDialog.show();
+
+    }
+
+    private String getFinalMessage() {
+        return String.format("Средняя скорость: %d симв/сек\n", exerciseHandler.getAverageSpeed()) +
+                String.format("Количество ошибок: %d/%d\n", exerciseHandler.getCurrentMistakes(), exerciseHandler.getMaxMistakes()) +
+                String.format("Время: %d c\n", timerValue);
     }
 
     private String getText(String old) {
