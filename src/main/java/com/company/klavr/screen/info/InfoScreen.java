7d831f4b7729726c8cb373e1fb53a496d8bc9bbe
Mix1e <109144752+Mix1e@users.noreply.github.com>
2022-12-28 21:17:09 +0400

    Справка доделал

--- src/main/java/com/company/klavr/screen/info/InfoScreen.java
+++ src/main/java/com/company/klavr/screen/info/InfoScreen.java
@@ -1,17 +1,13 @@
 package com.company.klavr.screen.info;
 
-import io.jmix.ui.Notifications;
 import io.jmix.ui.component.Button;
 import io.jmix.ui.component.MessageDialogFacet;
 import io.jmix.ui.screen.Screen;
 import io.jmix.ui.screen.Subscribe;
 import io.jmix.ui.screen.UiController;
 import io.jmix.ui.screen.UiDescriptor;
-import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebView;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.net.URL;
 
 @UiController("InfoScreen")
 @UiDescriptor("Info-screen.xml")
@@ -34,9 +30,10 @@ public class InfoScreen extends Screen {
                 String.format("Косарев Д.С.\n");
     }
     private void helpMe(){
-        WebView webView = new WebView();
-        WebEngine webEngine = webView.getEngine();
-        URL url = getClass().getResource("help.html");
-        webEngine.load(url.toExternalForm());
+//        WebView webView = new WebView();
+//        WebEngine webEngine = webView.getEngine();
+//        URL url = getClass().getResource("help.html");
+//        webEngine.load(url.toExternalForm());
+
     }
 }
